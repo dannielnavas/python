bc3133b537e8ae8434cad57a4f91398987689b06
Danniel Navas <dannielnavas@gmail.com>
2025-09-30 07:35:43 -0500

    Añadir script para calcular el área y perímetro de un círculo

---
+++ maths.py
@@ -0,0 +1,9 @@
+import math
+
+# Hallar el área de un círculo
+radius = 5
+area = math.pi * radius ** 2
+print(f"Área del círculo con radio {radius}: {area}")
+
+perimeter = 2 * math.pi * radius
+print(f"Perímetro del círculo con radio {radius}: {perimeter}")
